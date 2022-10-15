3bf30a9171d580be78d231c8ffe5303f348215e4
YuSidorin <xepo@mail.ru>
2022-10-15 18:58:26 +0300

    Initial commit

---
+++ src/test/java/com/fxpro/ipblocking/helper/Helper.java
@@ -0,0 +1,31 @@
+package com.fxpro.ipblocking.helper;
+
+import java.util.Properties;
+
+public class Helper {
+
+    public final Properties properties;
+    protected static final String VALIDIPLEFT = "0.0.0.0";
+    protected static final String VALIDIP = "192.168.1.22";
+    protected static final String VALIDIPRIGHT = "255.255.255.255";
+    protected static final String INVALIDIP = "256.255.255.255";
+    protected static final String WHITELISTEDIP = "104.192.138.232";
+    public static final String IP = "The IP ";
+    public static final String BLOCKED = " is blocked";
+    public static final String ISINVALID = " is invalid";
+    public static final String INVALIDCOMMAND = "Command is not valid";
+    public static final String WHITELISTEDIPCOMMAND = "The IP can't be blocked. It's in the whitelist.";
+
+    public Helper(Properties properties) {
+        this.properties = properties;
+    }
+
+    public String getPositiveResponseStringWithIp(String ipNum){
+        return IP + ipNum + BLOCKED;
+    }
+
+    public String getNegativeResponseStringWithIp(String ipNum){
+        return IP + ipNum + ISINVALID;
+    }
+
+}
---
+++ src/test/java/com/fxpro/ipblocking/test/IpBlockingTests.java
@@ -0,0 +1,120 @@
+package com.fxpro.ipblocking.test;
+
+import com.fxpro.ipblocking.helper.Helper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class IpBlockingTests extends Helper {
+
+    private static RequestSpecification spec;
+    public Response response;
+    public static String jsonAsString;
+
+    public IpBlockingTests(Properties properties) {
+        super(properties);
+    }
+
+    @BeforeAll
+    public static void initSpec(){
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri("http://localhost:8090/address")
+                .addFilter(new ResponseLoggingFilter())
+                .addFilter(new RequestLoggingFilter())
+                .build();
+    }
+
+    @AfterEach
+    void CleanUpBlockedAddresses(){
+        System.out.println("Cleaned up addresses");
+    }
+
+    @Test
+    public void testBlockValidIp(){
+        response =
+                given()
+                        .spec(spec)
+                        .when()
+                        .post(VALIDIP +"/block")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+        jsonAsString = response.asString();
+        assertEquals(jsonAsString, getPositiveResponseStringWithIp(VALIDIP));
+    }
+
+    @Test
+    public void testBlockInvalidIp(){
+        response =
+                given()
+                        .spec(spec)
+                        .when()
+                        .post( INVALIDIP +"/block")
+                        .then()
+                        .statusCode(400)
+                        .extract()
+                        .response();
+        jsonAsString = response.asString();
+        assertEquals(jsonAsString, getNegativeResponseStringWithIp(INVALIDIP));
+    }
+
+    @Test
+    public void testBlockInvalidCommand(){
+        response =
+                given()
+                        .spec(spec)
+                        .when()
+                        .post( VALIDIP +"/blocking")
+                        .then()
+                        .statusCode(400)
+                        .extract()
+                        .response();
+        jsonAsString = response.asString();
+        assertEquals(jsonAsString, Helper.INVALIDCOMMAND);
+    }
+
+    @Test
+    public void testRepeatBlockingIp(){
+        response =
+                given()
+                        .spec(spec)
+                        .when()
+                        .post( WHITELISTEDIP +"/block")
+                        .then()
+                        .statusCode(400)
+                        .extract()
+                        .response();
+        jsonAsString = response.asString();
+        assertEquals(jsonAsString, Helper.WHITELISTEDIPCOMMAND);
+    }
+
+    @Test
+    public void testBlockWhitelistedIp(){
+        response =
+                given()
+                        .spec(spec)
+                        .when()
+                        .post( WHITELISTEDIP +"/block")
+                        .then()
+                        .statusCode(400)
+                        .extract()
+                        .response();
+        jsonAsString = response.asString();
+        assertEquals(jsonAsString, Helper.WHITELISTEDIPCOMMAND);
+    }
+
+}
