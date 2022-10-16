952b3925ccb22ec038668ed4b03bf8cae620e3e8
YuSidorin <xepo@mail.ru>
2022-10-16 19:55:58 +0300

    add some tests

--- src/test/java/com/fxpro/ipblocking/tests/IpBlockingTests.java
+++ src/test/java/com/fxpro/ipblocking/tests/IpBlockingTests.java
@@ -26,21 +26,37 @@ public class IpBlockingTests extends Helper {
     public static void initSpec(){
         spec = new RequestSpecBuilder()
                 .setContentType(ContentType.JSON)
-                .setBaseUri("http://host.docker.internal:8090/address")
+                .setBaseUri("http://localhost:8090/address")
                 .addFilter(new ResponseLoggingFilter())
                 .addFilter(new RequestLoggingFilter())
                 .build();
     }
 
-//    @After
-//    public void CleanUpBlockedAddresses(){
-//        System.out.println("Cleaned up addresses");
-//    }
+    @After
+    public void CleanUpBlockedAddresses(){
+        System.out.println("Cleaned up addresses");
+    }
 
     @Test
     public void testBlockValidIp(){
-        jsonAsString = getBlockingValidIpResponse(BLOCK).asString();
+        jsonAsString = getBlockingIpResponse(BLOCK, VALIDIP).asString();
         assertEquals(jsonAsString, getPositiveResponseStringWithIp(VALIDIP, BLOCKED));
+        String jsonAsStringLeft = getBlockingIpResponse(BLOCK, VALIDIPLEFT).asString();
+        assertEquals(jsonAsStringLeft, getPositiveResponseStringWithIp(VALIDIPLEFT, BLOCKED));
+        String jsonAsStringRight = getBlockingIpResponse(BLOCK, VALIDIPRIGHT).asString();
+        assertEquals(jsonAsStringRight, getPositiveResponseStringWithIp(VALIDIPRIGHT, BLOCKED));
+    }
+
+    private Response getBlockingIpResponse(String block, String ip) {
+        return response =
+                given()
+                        .spec(spec)
+                        .when()
+                        .post(ip +"/" + block)
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
     }
 
     @Test
@@ -75,10 +91,10 @@ public class IpBlockingTests extends Helper {
 
     @Test
     public void testRepeatBlockingIp(){
-        response = getBlockingValidIpResponse(BLOCK);
+        response = getBlockingIpResponse(BLOCK, VALIDIP);
         jsonAsString = response.asString();
         assertEquals(jsonAsString, getPositiveResponseStringWithIp(VALIDIP, BLOCKED));
-        Response response2 = getBlockingValidIpResponse(BLOCKAGAIN);
+        Response response2 = getBlockingIpResponse(BLOCKAGAIN, VALIDIP);
         String jsonAsString2 = response2.asString();
         assertEquals(jsonAsString2, getPositiveResponseStringWithIp(VALIDIP, ALREADYBLOCKED));
     }
